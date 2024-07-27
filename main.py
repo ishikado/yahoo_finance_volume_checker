753922ef0144ff1953271d43715d2de7b3d65940
ishikado <okazakisaburo@gmail.com>
2024-07-28 00:37:37 +0900

    特定銘柄の平均出来高を計算するスクリプトを追加

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-def main():
-    print ("hello world")
+"""
+指定された銘柄の90日間の平均出来高を計算するやつ
+"""
 
-main()
+import yfinance as yf
+
+ticker = "AAPL"
+data = yf.download(ticker, period='91d', interval = "1d", threads = False)
+sum_volume = 0
+today_volume = 0
+first = True
+cnt = 0
+for index, row in data.iterrows():
+    volume = row["Volume"]
+    if first:
+        first = False
+        today_volume = volume
+    else:
+        cnt += 1
+        sum_volume += volume
+
+avg_volume = sum_volume / cnt
+
+print ("today_volume = " + str(today_volume))
+print ("avg_volume = " + str(avg_volume))
