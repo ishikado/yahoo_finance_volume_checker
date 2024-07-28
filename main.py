f6132ac6012791990deed32d4e72cef4484acd75
ishikado <okazakisaburo@gmail.com>
2024-07-28 10:05:42 +0900

    url を出力するように修正

--- main.py
+++ main.py
@@ -4,22 +4,31 @@
 
 import yfinance as yf
 
+
+def check(ticker):
+    data = yf.download(ticker, period='3mo', interval = "1d", threads = False)
+    sum_volume = 0
+    today_volume = 0
+    first = True
+    cnt = 0
+    for index, row in data.iterrows():
+        #print (index)
+        volume = row["Volume"]
+        if first:
+            first = False
+            today_volume = volume
+        else:
+            cnt += 1
+            sum_volume += volume
+    avg_volume = sum_volume / cnt
+    # for debug
+    print ("today_volume = " + str(today_volume))
+    print ("avg_volume = " + str(avg_volume))
+    return today_volume >= avg_volume
+    
+
+
 ticker = "AAPL"
-data = yf.download(ticker, period='91d', interval = "1d", threads = False)
-sum_volume = 0
-today_volume = 0
-first = True
-cnt = 0
-for index, row in data.iterrows():
-    volume = row["Volume"]
-    if first:
-        first = False
-        today_volume = volume
-    else:
-        cnt += 1
-        sum_volume += volume
+if check(ticker):
+    print (ticker + " https://finance.yahoo.com/quote/" + ticker + "/")
 
-avg_volume = sum_volume / cnt
-
-print ("today_volume = " + str(today_volume))
-print ("avg_volume = " + str(avg_volume))
