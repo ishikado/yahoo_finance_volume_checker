6b70dd84b8286611493018d19824615a0e11142c
ishikado <okazakisaburo@gmail.com>
2024-07-29 01:04:13 +0900

    - csv から銘柄一覧を読み込めるように修正 - 出来高を予測する機能を実装

--- main.py
+++ main.py
@@ -3,36 +3,79 @@
 """
 
 import yfinance as yf
+import time
+import sys
+import datetime
 
+
+def calc_elappsed_time_ratio(now_min):
+    # TODO: 夏時間も自動で切り替えたい
+    # 3月の第二日曜日から11月の第一日曜日までらしい
+    is_summer = True
+    if is_summer:
+        start_min = 8 * 60 + 30
+        end_min = 15 * 60
+    else:
+        start_min = 9 * 60 + 30
+        end_min = 16 * 60
+
+    elappsed_time_ratio = 1.0
+    if start_min <= now_min and now_min <= end_min:
+        elappsed_time_ratio = 1.0 * (end_min - start_min) /  (now_min - start_min)
+    return elappsed_time_ratio
+    
 
 def check(ticker):
+    # TODO: 
+    # yf.Ticker(ticker).info とかで平均出来高含めて情報が取れるので、そっちで処理したほうがよさそう
+    # 現在の株価も current Price みたいなデータで取れそう？
     data = yf.download(ticker, period='3mo', interval = "1d", threads = False)
     sum_volume = 0
     today_volume = 0
     first = True
     cnt = 0
     for index, row in data.iterrows():
-        #print (index)
         volume = row["Volume"]
-        if first:
-            first = False
-            today_volume = volume
-        else:
-            cnt += 1
-            sum_volume += volume
+        cnt += 1
+        sum_volume += volume
+
+    # TODO: 現在のリアルタイムの出来高が取れるか若干微妙なので調査
+    today_volume = row["Volume"]
+    sum_volume -= today_volume
+    cnt -= 1
+
     avg_volume = sum_volume / cnt
+    
+    # ニューヨークのタイムゾーンで現在時刻を取得
+    nytimenow = datetime.datetime.now(tz = datetime.timezone(datetime.timedelta(hours=-4)))
+    now_min = nytimenow.hour * 60 + nytimenow.minute
+
+    expected_today_volume = calc_elappsed_time_ratio(now_min) * today_volume
+
     # for debug
-    print ("today_volume = " + str(today_volume))
-    print ("avg_volume = " + str(avg_volume))
-    return today_volume >= avg_volume
+    #print (ticker)
+    #print ("today_volume = " + str(today_volume))
+    #print ("avg_volume = " + str(avg_volume))
+    return expected_today_volume >= avg_volume
     
 
 def output_url(ticker):
     print (ticker + " https://finance.yahoo.com/quote/" + ticker + "/")
 
 def main():
-    ticker = "AAPL"
-    if check(ticker):
+    tickers = []
+    for line in sys.stdin:
+        tickers.append(line.split(",")[0])
+    tickers = tickers[1:]
+    results = []
+    for ticker in tickers:
+        # TODO: 株価が取れない場合があるので例外処理を入れたい
+        if check(ticker):
+            results.append(ticker)
+        time.sleep(3)
+
+    for ticker in results:
         output_url(ticker)
+    
 
 main()
