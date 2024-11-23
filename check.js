18e6a5184f334b26d54d8d8f2e0a531785ec38c3
ishikado <okazakisaburo@gmail.com>
2024-11-23 11:34:08 +0900

    yahoo finance のチェックスクリプトを修正

--- check.js
+++ check.js
@@ -35,20 +35,19 @@ function toNumber(number_string) {
 
 function main() {
     if (location.href.startsWith('https://finance.yahoo.com/portfolio/')) {
-        var es = document.querySelectorAll('tr[class^="simpTblRow Bgc($hoverBgColor):h BdB Bdbc($seperatorColor) Bdbc($tableBorderBlue):h H(32px)"]');
+        var es = document.querySelectorAll('tr[class="row yf-1z0ossw"]');
         var result = ""
         for (i = 0; i < es.length; i++) {
-            symbol = es[i].querySelector('td[aria-label="Symbol"]').querySelector('a[data-test="quoteLink"]').innerHTML
-            rawVolumeFinStreamer = es[i].querySelector('td[aria-label="Volume"]').querySelector('fin-streamer')
-            // rawVolume の span 要素がなく、直接 innnerHTML で値が入っている場合があるのでその対応
-            if (rawVolumeFinStreamer.querySelector('span') == null) {
-                rawVolume = rawVolumeFinStreamer.innerHTML
-            } else {
-                rawVolume = rawVolumeFinStreamer.querySelector('span').innerHTML
-            }
-            rawAvgVolume = es[i].querySelector('td[aria-label^="Avg Vol"]').innerHTML
-            rawChange = es[i].querySelector('td[aria-label="Change"]').querySelector('fin-streamer').querySelector('span').innerHTML
-            rawChangePer = es[i].querySelector('td[aria-label^="Chg "]').querySelector('fin-streamer').querySelector('span').innerHTML
+            symbol = es[i].querySelector('td[class="yf-1z0ossw lpin"]')
+                            .querySelector('div[style^=display]')
+                            .querySelector('a[class="loud-link fin-size-medium yf-1e4diqp"]').innerHTML
+
+            // symbol 以外の要素は class 名など attr 要素で一意に特定できないため、td で絞って順番に要素をなめている、yahoo finance 側の実装で順番が変わると機能しなくなるので注意
+            tds = es[i].querySelectorAll('td[class=" yf-1z0ossw"]')
+            rawVolume = tds[5].innerHTML
+            rawAvgVolume = tds[7].innerHTML
+            rawChange = tds[2].querySelector('span[class^="base"]').innerHTML
+            rawChangePer = tds[1].querySelector('span[class^="base"]').innerHTML
 
             volume = toNumber(rawVolume)
             avg_volume = toNumber(rawAvgVolume)
@@ -63,7 +62,7 @@ function main() {
                 result += symbol + " " + rawChangePer
                 result += "\n"
                 // 該当要素の背景色をかえる
-                es[i].querySelector('td[aria-label="Symbol"]').setAttribute("style", "background: #00ff00")
+                es[i].querySelector('td[class="yf-1z0ossw lpin"]').setAttribute("style", "background: #00ff00")
             }
         }
         alert(result)
